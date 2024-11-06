3c27921e31c027976bd4f530043758e872ea58e7
Devan Shepard <wdevanshepard@gmail.com>
2024-11-05 21:19:12 -0600

    Add files via upload

---
+++ 2-Subnets.tf
@@ -0,0 +1,85 @@
+#These are   for  public
+
+resource "aws_subnet" "public-us-east-2a" {
+  vpc_id                  = aws_vpc.app1.id
+  cidr_block              = "10.187.1.0/24"
+  availability_zone       = "us-east-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "public-us-east-2a"
+    Service = "application1"
+    Owner   = "MFNerd"
+    Planet  = "Uranus"
+  }
+}
+
+resource "aws_subnet" "public-us-east-2b" {
+  vpc_id                  = aws_vpc.app1.id
+  cidr_block              = "10.187.2.0/24"
+  availability_zone       = "us-east-2b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "public-us-east-2b"
+    Service = "application1"
+    Owner   = "MFNerd"
+    Planet  = "Uranus"
+  }
+}
+
+
+resource "aws_subnet" "public-us-east-2c" {
+  vpc_id                  = aws_vpc.app1.id
+  cidr_block              = "10.187.3.0/24"
+  availability_zone       = "us-east-2c"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name    = "public-us-east-2c"
+    Service = "application1"
+    Owner   = "MFNerd"
+    Planet  = "Uranus"
+  }
+}
+
+#these are for private
+resource "aws_subnet" "private-us-east-2a" {
+  vpc_id            = aws_vpc.app1.id
+  cidr_block        = "10.187.11.0/24"
+  availability_zone = "us-east-2a"
+
+  tags = {
+    Name    = "private-us-east-2a"
+    Service = "application1"
+    Owner   = "MFNerd"
+    Planet  = "Uranus"
+  }
+}
+
+resource "aws_subnet" "private-us-east-2b" {
+  vpc_id            = aws_vpc.app1.id
+  cidr_block        = "10.187.12.0/24"
+  availability_zone = "us-east-2b"
+
+  tags = {
+    Name    = "private-us-east-2b"
+    Service = "application1"
+    Owner   = "MFNerd"
+    Planet  = "Uranus"
+  }
+}
+
+
+resource "aws_subnet" "private-us-east-2c" {
+  vpc_id            = aws_vpc.app1.id
+  cidr_block        = "10.187.13.0/24"
+  availability_zone = "us-east-2c"
+
+  tags = {
+    Name    = "private-us-east-2c"
+    Service = "application1"
+    Owner   = "MFNerd"
+    Planet  = "Uranus"
+  }
+}
---
+++ 6-SG01-All.tf
@@ -0,0 +1,61 @@
+resource "aws_security_group" "app1-sg01-servers" {
+  name        = "app1-sg01-servers"
+  description = "app1-sg01-servers"
+  vpc_id      = aws_vpc.app1.id
+
+ingress {
+    description = "MyHomePage"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "app1-sg01-servers"
+    Service = "application1"
+    Owner   = "MFNerd"
+    Planet  = "Uranus"
+  }
+
+}
+
+
+
+
+
+resource "aws_security_group" "app1-sg02-LB01" {
+  name        = "app1-sg02-LB01"
+  description = "app1-sg02-LB01"
+  vpc_id      = aws_vpc.app1.id
+
+  ingress {
+    description = "MyHomePage"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name    = "app1-sg02-LB01"
+    Service = "application1"
+    Owner   = "MFNerd"
+    Planet  = "Uranus"
+  }
+
+}
